2084da53a5666098ff64b1a9a8b4c46776699363
kipngenokevin <kipngenokevin254@gmail.com>
2023-08-23 02:05:02 +0300

    Task 1 first attempt

--- print-int.c
+++ print-int.c
@@ -6,6 +6,42 @@
  */
 int print_int(int num)
 {
-	num = 1;
-	return (num);
+	int num_len = 0;
+	int temp = num;
+	int count = 0;
+	int i;
+
+	if (num < 0)
+	{
+		print_char('-');
+		count++;
+		num = -num;
+	}
+	while (temp != 0)
+	{
+		num_len++;
+		temp /= 10;
+	}
+	if (num == 0)
+	{
+		print_char('0');
+		count++;
+	}
+	else
+	{
+		int divisor = 1;
+
+		for (i = 1; i < num_len; i++)
+			divisor *= 10;
+		while (divisor != 0)
+		{
+			int digit = num / divisor;
+
+			print_char('0' + digit);
+			count++;
+			num -= digit * divisor;
+			divisor /= 10;
+		}
+	}
+	return (count);
 }
