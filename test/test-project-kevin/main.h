0223f9f845a063b36cb82889d7c837b683bbf02b
kipngenokevin <kipngenokevin254@gmail.com>
2023-08-22 12:55:28 +0300

    unknown format

--- test/test-project-kevin/main.h
+++ test/test-project-kevin/main.h
@@ -7,4 +7,5 @@ int _printf(char *format, ...);
 void print_char(char c);
 void print_string(char *str);
 void handle_format(char **format, va_list args, int *count);
+int handle_unknown(char c);
 #endif
